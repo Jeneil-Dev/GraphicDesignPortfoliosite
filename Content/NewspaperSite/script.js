66c268a83623658e070af73eca84b84c721d0911
Jeneil Henry <shenry.jeneil@gmail.com>
2024-01-21 11:41:27 -0500

    graphic deign portfolio

---
+++ Content/NewspaperSite/script.js
@@ -0,0 +1,5 @@
+document.getElementById("heroButtion").addEventListener("click",myFunction);
+function myFunction(){
+  alert ("input information to subscribe!!");
+  document.getElementsByClassName("form").style.display = "contents";
+}
